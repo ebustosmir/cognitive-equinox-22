9c654d592da87b0486b4d9498371960b9be31d2c
Samuel Ramirez <samuel.ramirezberdun@telefonica.com>
2022-10-06 16:43:07 +0200

    Update

--- host-mock/server.py
+++ host-mock/server.py
@@ -1,10 +1,18 @@
+from datetime import datetime
+import pytz
 from flask import Flask
 import os
 
 app = Flask(__name__)
 SERVER_NAME = os.getenv("SERVER_NAME")
-
+FORMAT = "%d/%m/%Y %H:%M:%S"
+TZ = pytz.timezone('Europe/Madrid')
 
 @app.route('/')
+def root_path():
+    return 'Call to /hello'
+
+
+@app.route('/hello')
 def hello():
-    return '%s' % SERVER_NAME
+    return {'name': SERVER_NAME, 'time': datetime.now(TZ).strftime(FORMAT)}
---
+++ tools/variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export NETWORK_NAME="network"
+export HOST1_NAME='serversito1'
+export HOST2_NAME='serversito2'
+export HOST3_NAME='serversito3'
+export HOST4_NAME='serversito4'
+export HOST5_NAME='serversito5'
