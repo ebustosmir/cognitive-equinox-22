7fd413f2c895d6b07de95b29954908bbe55d3123
Samuel Ramirez <samuel.ramirezberdun@telefonica.com>
2022-10-06 22:46:42 +0200

    Updates

--- network-manager/docker_build.sh
+++ network-manager/docker_build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_ROOT_FOLDER=${SCRIPT_FOLDER}/..
 
-docker build -t network-manager .
+docker build -t network-manager ${PROJECT_ROOT_FOLDER}/network-manager
