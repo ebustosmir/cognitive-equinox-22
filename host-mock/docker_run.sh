e0f3691bd3ed040c8bb5126ef2bf25475eadc431
ebustos <eduardo.bustosmiranda@telefonica.com>
2022-10-06 15:15:51 +0200

    wip

--- host-mock/docker_run.sh
+++ host-mock/docker_run.sh
@@ -1,3 +1,3 @@
-docker run --name mock1 -d -p 5000:5000 -e FLASK_APP=server -e SERVER_NAME=serversito host-mock
+docker run --name mock1 -d -p 8000:5000 -e FLASK_APP=server -e SERVER_NAME=serversito host-mock
 
 docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mock1
