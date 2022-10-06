45a73786d29c4cf41ff62fbf4599dd3247584988
ebustos <eduardo.bustosmiranda@telefonica.com>
2022-10-06 20:39:45 +0200

    wip network-manager

---
+++ network-manager/docker_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_ROOT_FOLDER=${SCRIPT_FOLDER}/..
+
+. "${PROJECT_ROOT_FOLDER}/tools/variables.sh"
+
+docker rm -f ${HOST1_NAME} ${HOST2_NAME} ${HOST3_NAME} ${HOST4_NAME} ${HOST5_NAME}
+
+. ${PROJECT_ROOT_FOLDER}/tools/start_networks.sh
+
+docker run --name network-manager --network "${NETWORK_NAME}" --dns=172.20.0.2 --ip=172.20.0.3 -d -p 6001:5000 -e FLASK_APP=server network-manager
