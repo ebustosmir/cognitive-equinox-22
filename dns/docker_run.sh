31d5a659979b9e708f3461d39896b54b6a21af5a
jopime <jose.pimentelmesones@telefonica.com>
2022-10-07 00:07:31 +0200

    [[feat]] Logs as volume

--- dns/docker_run.sh
+++ dns/docker_run.sh
@@ -4,4 +4,4 @@ PROJECT_ROOT_FOLDER=${SCRIPT_FOLDER}/..
 
 docker rm -f dns-server || true
 
-docker run -d --name=dns-server --net=${NETWORK_NAME} -v "${PROJECT_ROOT_FOLDER}/volume/db.cognitive-equinox.com":/etc/bind/zones/db.cognitive-equinox.com --ip=172.20.0.2 bind9
+docker run -d --name=dns-server --net=${NETWORK_NAME} -v "${PROJECT_ROOT_FOLDER}/volume/logs":/var/log/bind -v "${PROJECT_ROOT_FOLDER}/volume/db.cognitive-equinox.com":/etc/bind/zones/db.cognitive-equinox.com --ip=172.20.0.2 bind9
