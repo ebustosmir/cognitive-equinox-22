0430370482cffc2ac555c6958c00c896fd97365f
DaniMG95 <daniel.munozgonzalez@telefonica.com>
2022-10-06 21:42:58 +0200

    update class

--- src/dns_host_handler.py
+++ src/dns_host_handler.py
@@ -1,23 +1,42 @@
 
-class FileHandler:
+class DnsHostHandler:
 
     def __init__(self, filename: str):
         self.__filename = filename
 
-    def write_host(self, hostname: str, host: str):
+    def check_exist_dns(self, hostname: str, domain: str) -> bool:
+        with open(self.__filename, mode='r') as file:
+            for line in file:
+                if hostname + domain in line:
+                    return True
+        return False
+
+    def append_host(self, hostname: str, ip: str, domain: str):
         with open(self.__filename, mode='a') as file:
-            file.write('\n%s.cognitive-equinox.com.        IN      A      %s' % (hostname, host))
+            file.write('\n%s.%s        IN      A      %s' % (hostname, domain, ip))
 
-    def delete_host(self, hostname: str):
-        with open(self.__filename, 'r+') as file:
-            lines = file.readlines()
-            file.seek(0)
-            for line in lines:
-                if hostname + '.cognitive-equinox.com.' not in line:
-                    file.write(line)
-            file.truncate()
+    def write_host(self, hostname: str, ip: str, domain: str) -> None:
+        if not self.check_exist_dns(hostname=hostname, domain=domain):
+            self.append_host(hostname=hostname, ip=ip, domain=domain)
+        else:
+            self.delete_host(hostname=hostname, domain=domain)
+            self.append_host(hostname=hostname, ip=ip, domain=domain)
 
-    def list_host(self):
+    def delete_host(self, hostname: str, domain: str) -> None:
+        if self.check_exist_dns(hostname=hostname, domain=domain):
+            definition_hosts = False
+            with open(self.__filename, 'r') as f:
+                lines = f.readlines()
+            with open(self.__filename, 'w') as f:
+                for line in lines:
+                    if '; name servers - A records' in line:
+                        definition_hosts = True
+                    if not definition_hosts:
+                        f.write(line)
+                    elif hostname + domain not in line:
+                        f.write(line)
+
+    def list_host(self) -> None:
         hosts = {}
         read = False
         with open(self.__filename, 'r') as file:
@@ -25,16 +44,20 @@ class FileHandler:
                 if '; name servers - A records' in line:
                     read = True
                 elif read:
-                    host = line.split('IN')[0].rsplit()[0]
-                    ip = line.split('A')[1].rsplit()[0]
-                    hosts[host] = ip
+                    if line.strip():
+                        host = line.split('IN')[0].rsplit()[0]
+                        ip = line.split('A')[1].rsplit()[0]
+                        hosts[host] = ip
         return hosts
 
 
 if __name__ == "__main__":
-    file_handler = FileHandler('../dns/etc/db.cognitive-equinox')
-    file_handler.write_host(hostname='host3', host='172.44.2.1')
-    file_handler.delete_host(hostname="ns1")
+    file_handler = DnsHostHandler('dns/etc/db.cognitive-equinox')
+    file_handler.write_host(hostname='host3', ip='172.44.2.1', domain='.cognitive-equinox.com.')
+    file_handler.write_host(hostname='host4', ip='172.44.2.9', domain='.cognitive-equinox.com.')
+    file_handler.delete_host(hostname="host1", domain='.cognitive-equinox.com.')
+    file_handler.delete_host(hostname="ns1", domain='.cognitive-equinox.com.')
+    file_handler.delete_host(hostname='host3', domain='.cognitive-equinox.com.')
     hosts = file_handler.list_host()
     print(hosts)
 
